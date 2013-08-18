521fe44441ada71ccc6726b478e774fb53fe036c
arthur-feral <arthur.feral@hotmail.fr>
2013-08-18 23:21:36 +0200

    Update actions.js

--- actions.js
+++ actions.js
@@ -17,67 +17,55 @@ actions = $.extend(actions, {
             fn = fn[nss[0]];
             nss.shift();
         }
-        if(nss.length==1)fn = fn[nss[0]];
+        if(nss.length===1)fn = fn[nss[0]];
         if(fn) (fn)(el, e);
     }
 });
 
-$.fn.bindActions = function(is){
-    var ev = [
-        'click',
-        'keydown',
-        "keyup",
-        'change'
-    ];
-    var evName = [
-        "bton",
-        "key",
-        "key",
-        "change"
-    ];
+$.fn.bindActions = function(params){
     function bindDom(){
-        $(document).on('DOMNodeInserted.actions', function(e){
+        $(document).on('DOMNodeInserted.action', '.action', function(e){
+            console.log('gzer')
             var t = $(e.target);
             var isTarget = t.data('action') != undefined && t.data('event') != undefined;
             if(isTarget){
                 t.bindActions('on');
-                console.info('Bind action on new node', t);
+                //console.info('Bind action on ', t);
             }
         });
     }
-    if($(document).data('events')['DOMNodeInserted'] == undefined){
-        bindDom();
-    }else{
-        if($(document).data('events')['DOMNodeInserted'][0].namespace != "actions"){
+    if($(document).data('events') !== undefined){
+        if($(document).data('events')['DOMNodeInserted'] === undefined){
             bindDom();
+        }else{
+            if($(document).data('events')['DOMNodeInserted'][0].namespace != "action"){
+                bindDom();
+            }
         }
     }
-    if(is == 'on'){
-    	return this.each(function(){
+    else{
+        bindDom();
+    }
+    if(this.length === 0) return;
+    function bindOn (elems) {
+        elems.each(function(){
             if(!$(this).hasClass('inactive')){
-        		var a = "";
-	        	var currentEv = "";
-	        	var currentEvName = "";
-	            for(var i = 0 ; i < ev.length ; i++){
-	                if($(this).attr('data-event') == ev[i]){
-	                	a = ev[i]+'.'+evName[i];
-	                	currentEv = ev[i];
-	                	currentEvName = evName[i];
-	                }
-	            }
-	            if($(this).data('events') != undefined && $(this).data('events')[currentEv] != undefined){
-	            	for(var i = 0 ; i < $(this).data('events')[currentEv].length ; i++){
-		            	if($(this).data('events')[currentEv][i].namespace == currentEvName){
-                            console.warn('element has already an event, rebinding...');
-		            		// console.warn($(this), $(this).data('events')[currentEv][i], $(this).attr('data-action'));
+                var currentEv = $(this).attr('data-event');
+                var a = currentEv+'.action';
+                
+                if($(this).data('events') != undefined && $(this).data('events')[currentEv] != undefined){
+                    for(var i = 0 ; i < $(this).data('events')[currentEv].length ; i++){
+                        if($(this).data('events')[currentEv][i].namespace === 'action'){
+                            console.error('element has already an event, rebinding...');
+                            // console.warn($(this), $(this).data('events')[currentEv][i], $(this).attr('data-action'));
                             // $(this).off(currentEvName);
-		            		$(this).off(a);
-		            		break;
-		            	}
-		            }
-	            }
-	            action = $(this).attr('data-action');
-                if(action == undefined){
+                            $(this).off(a);
+                            break;
+                        }
+                    }
+                }
+                action = $(this).attr('data-action');
+                if(action === undefined){
                     console.error($(this), 'has no function defined')
                 }
                 if(action != undefined){
@@ -88,8 +76,8 @@ $.fn.bindActions = function(is){
                         fn = fn[nss[0]];
                         nss.shift();
                     }
-                    if(nss.length==1){
-                        if(fn == undefined) fn = {};
+                    if(nss.length===1){
+                        if(fn === undefined) fn = {};
                         fn = fn[nss[0]];
                     }
                     $(this).on(a, function(e){
@@ -101,20 +89,25 @@ $.fn.bindActions = function(is){
                         }
                     });
                 }
-	        }
-        });
-    }
-    if(is == "off"){
-    	return this.each(function(){
-            if(!$(this).hasClass('inactive')){
-	    		var a = "";
-	            for(var i = 0 ; i < ev.length ; i++){
-	                if($(this).attr('data-event') == ev[i]){
-	                	a = ev[i]+'.'+evName[i];
-	                }
-	            }
-	            $(this).off(a);
             }
         });
-    }   
+    }
+    function bindOff (elems) {
+        elems.each(function(){
+            if(!$(this).hasClass('inactive')){
+                var a = $(this).attr('data-event')+'.action';
+                $(this).off(a);
+            }
+        });
+    }
+    
+    //cas on est plus en train de binder/unbinder un/des elem(s)
+    if(typeof params === 'string'){
+        if(params === 'on'){
+            return bindOn(this);
+        }
+        if(params === 'off'){
+            return bindOff(this);
+        }
+    }
 };
